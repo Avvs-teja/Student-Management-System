5342b96d550c3f89c30247a32984d5eaec50af9e
Avvs-teja <124860118+Avvs-teja@users.noreply.github.com>
2024-05-08 15:32:38 +0530

    Add files via upload

---
+++ studentCrud/myapp/models.py
@@ -0,0 +1,12 @@
+# models.py
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
+    email = models.EmailField()
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
